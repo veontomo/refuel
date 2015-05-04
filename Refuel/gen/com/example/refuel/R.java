db9eae0bd1337899c98fcf79bbac3fab7517e813
Mario Rossi <veontomo@gmail.com>
2015-05-04 11:37:18 +0200

    feat: create template for fuel data insertion

--- Refuel/gen/com/example/refuel/R.java
+++ Refuel/gen/com/example/refuel/R.java
@@ -25,7 +25,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int buttonCancel=0x7f080006;
+        public static final int buttonSave=0x7f080005;
+        public static final int fuelStation=0x7f080004;
+        public static final int km=0x7f080000;
+        public static final int paid=0x7f080003;
+        public static final int price=0x7f080001;
+        public static final int quantity=0x7f080002;
     }
     public static final class layout {
         public static final int activity_refuel_data=0x7f030000;
@@ -34,9 +41,20 @@ public final class R {
         public static final int refuel_data=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f05000d;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int buttonCancel=0x7f05000c;
+        public static final int buttonSave=0x7f05000b;
+        public static final int fuelStation=0x7f050009;
+        public static final int fuelStationHint=0x7f05000a;
+        public static final int km=0x7f050001;
+        public static final int kmHint=0x7f050002;
+        public static final int paid=0x7f050007;
+        public static final int paidHint=0x7f050008;
+        public static final int price=0x7f050003;
+        public static final int priceHint=0x7f050004;
+        public static final int quantity=0x7f050005;
+        public static final int quantityHint=0x7f050006;
     }
     public static final class style {
         /** 
