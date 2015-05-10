d8ccabedea5e28d6887692f4364c479b8d3365c4
Mario Rossi <veontomo@gmail.com>
2015-05-10 18:06:36 +0200

    test: add first test. First try.

---
+++ Refuel/src/com/veontomo/refuel/test/ModelTest.java
@@ -0,0 +1,14 @@
+package com.veontomo.refuel.test;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Andrea on 10/05/2015.
+ */
+public class ModelTest extends TestCase {
+    public void test() throws Exception {
+        final int expected = 1;
+        final int reality = 1;
+        assertEquals(expected, reality);
+    }
+}
