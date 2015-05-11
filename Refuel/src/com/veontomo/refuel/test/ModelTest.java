a829b9dddbf539836dc9ae95df83a3bed148fa51
Mario Rossi <veontomo@gmail.com>
2015-05-11 11:44:20 +0200

    test: check id of existing model

--- Refuel/src/com/veontomo/refuel/test/ModelTest.java
+++ Refuel/src/com/veontomo/refuel/test/ModelTest.java
@@ -4,6 +4,7 @@ import com.veontomo.refuel.Model;
 
 import junit.framework.TestCase;
 
+
 /**
  * Model's class tests
  * @author veontomo@gmail.com
@@ -23,5 +24,15 @@ public class ModelTest extends TestCase {
         Foo foo = new Foo();
         assertEquals("wrong name attribute in subclass", foo.getName(), "Foo");
     }
+
+    public void testGetIdOfNewRecord() throws Exception {
+        assertNull("id must be null of a new record", this.model.getId());
+    }
+
+    public void testGetIdOfExistingRecord() throws Exception {
+        this.model.save();
+        assertNotNull("id must be not null of existing record", this.model.getId());
+    }
+
 
 }
